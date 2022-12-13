1c67a0d7ff3459b0aef37ca15cdf3e0aa9a9a958
Liv Nelson <livnelson@me.com>
2022-12-13 15:21:56 -0800

    commit tree f43685d2ae2f72e40292f1172b799b130d44ac00

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true, length: { minimum: 250 }
+    validates :summary, presence: true, length: { maximum: 250 }
+    validates :category, presence: true, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :clickbait
+
+    def clickbait
+        title_includes = [ /Won't Believe/, /Secret/, /Top \d/, /Guess/ ]
+        if title_includes.none? { |t| t.match title }
+            errors.add(:title, "must be clickbait")
+    end
+  end
 end
